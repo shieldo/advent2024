d0f8227a1baacbceea02b0e0e3b49551043707ca
Douglas Greenshields <dgreenshields@gmail.com>
2024-12-01 12:48:38 +0000

    part 2 of day 1

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -1,10 +1,34 @@
 fn main() {
     let input = include_str!("input.txt");
     println!("Answer to part 1: {}", combine_lists(input));
+    println!("Answer to part 2: {}", sum_counts(input));
 }
 
 fn combine_lists(input: &str) -> usize {
-    let numbers = input.lines().map(|line| line.split_ascii_whitespace().map(|digits| digits.parse::<usize>().unwrap()).collect::<Vec<_>>());
+    let (list1, list2) = parsed_lists(input);
+
+    list1
+        .iter()
+        .zip(list2.iter())
+        .map(|(&a, &b)| (a as isize - b as isize).abs() as usize)
+        .sum()
+}
+
+fn sum_counts(input: &str) -> usize {
+    let (list1, list2) = parsed_lists(input);
+
+    list1
+        .iter()
+        .map(|num| *num * list2.iter().filter(|&item| num == item).count())
+        .sum()
+}
+
+fn parsed_lists(input: &str) -> (Vec<usize>, Vec<usize>) {
+    let numbers = input.lines().map(|line| {
+        line.split_ascii_whitespace()
+            .map(|digits| digits.parse::<usize>().unwrap())
+            .collect::<Vec<_>>()
+    });
     let mut list1 = vec![];
     let mut list2 = vec![];
     for pair in numbers {
@@ -13,7 +37,7 @@ fn combine_lists(input: &str) -> usize {
     }
     list1.sort();
     list2.sort();
-    list1.iter().zip(list2.iter()).map(|(&a, &b)| (a as isize - b as isize).abs() as usize).sum()
+    (list1, list2)
 }
 
 #[cfg(test)]
@@ -34,5 +58,10 @@ mod tests {
     #[rstest]
     fn test_combine_lists(test_input: &str) {
         assert_eq!(combine_lists(test_input), 11);
+    }
+
+    #[rstest]
+    fn test_sum_counts(test_input: &str) {
+        assert_eq!(sum_counts(test_input), 31);
     }
 }
