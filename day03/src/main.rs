173c8bace6362a1a879ad8bd19816a5f5fc2c0ea
Douglas Greenshields <douglas.greenshields@communities.gov.uk>
2024-12-03 10:46:53 +0000

    part 2 of day 3

--- day03/src/main.rs
+++ day03/src/main.rs
@@ -3,6 +3,7 @@ use regex::Regex;
 fn main() {
     let input = include_str!("input.txt");
     println!("Answer to part 1: {}", part1(input));
+    println!("Answer to part 2: {}", part2(input));
 }
 
 fn part1(input: &str) -> usize {
@@ -10,6 +11,14 @@ fn part1(input: &str) -> usize {
         .unwrap()
         .captures_iter(input)
         .map(|c| &c[1].parse::<usize>().unwrap() * &c[2].parse::<usize>().unwrap())
+        .sum()
+}
+
+fn part2(input: &str) -> usize {
+    Regex::new(r"(?ms)don't\(\).*?do\(\)")
+        .unwrap()
+        .split(input)
+        .map(part1)
         .sum()
 }
 
@@ -26,5 +35,15 @@ mod tests {
     #[rstest]
     fn test_part1(test_input: &str) {
         assert_eq!(part1(test_input), 161);
+    }
+
+    #[fixture]
+    fn test_input2() -> &'static str {
+        "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"
+    }
+
+    #[rstest]
+    fn test_part2(test_input2: &str) {
+        assert_eq!(part2(test_input2), 48);
     }
 }
